d22445cd10a139a666b319126039a0c517a07c9a
Bruce Steedman <bruce.steedman@gmail.com>
2016-05-30 16:51:16 +0100

    add metadata method

--- lib/arcrest/catalog.rb
+++ lib/arcrest/catalog.rb
@@ -1,8 +1,34 @@
+require 'net/http'
+require 'json'
+
 module ArcREST
   # a catalog of services
   class Catalog
-    def init
+    attr_reader :metadata
+    PATH = '/arcgis/rest/services'.freeze
 
+    def initialize(host)
+      @host = host
+      @uri = uri
+      json_param
+      @metadata = metadata
+    end
+
+    def uri
+      raise ArgumentError, 'Invalid url' if URI(@host).path.casecmp(PATH) != 0
+      URI @host
+    end
+
+    def json_param
+      @uri.query = URI.encode_www_form(f: 'json')
+    end
+
+    def metadata
+      JSON.parse get
+    end
+
+    def get
+      Net::HTTP.get @uri
     end
   end
 end
--- spec/arcrest/catalog_spec.rb
+++ spec/arcrest/catalog_spec.rb
@@ -1,11 +1,32 @@
-require 'arcrest'
+v10_21_server_endpoint = 'http://rmgsc.cr.usgs.gov/ArcGIS/rest/services'
 
 describe ArcREST do
-  let(:catalog) { ArcREST::Catalog.new }
+  let(:catalog) { ArcREST::Catalog.new(v10_21_server_endpoint) }
+  let(:bad_url) { ArcREST::Catalog.new('bad_url') }
 
-  context '#new' do
-    it 'creates an instance of the class' do
-      expect(catalog.class).to eq ArcREST::Catalog
+  context '#new(endpoint)' do
+    context 'with a valid endpoint address' do
+      it 'creates an instance of the class' do
+        expect(catalog.class).to eq ArcREST::Catalog
+      end
+    end
+
+    context 'with an invalid endpoint address' do
+      it 'raises ArgumentError "Invalid endpoint address"' do
+        expect(-> { bad_url }).to raise_error(ArgumentError, 'Invalid url')
+      end
+    end
+  end
+
+  context '#metadata' do
+    it 'returns a hash of metadata' do
+      expect(catalog.metadata.class).to eq Hash
+    end
+
+    context 'whose keys' do
+      it 'are ' do
+        expect(catalog.metadata.keys).to include('currentVersion', 'services')
+      end
     end
   end
 end
